9b3abd6a84f890cd35d519643f925db5e786c306
DezmenSykes <oddly.observant@gmail.com>
2024-07-30 12:10:26 -0500

    error and 404 page fix.

---
+++ app/not-found.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (
+        <div>
+            <h2>404</h2>
+            <p>Sorry, the requested page does not exist my guy.</p>
+        </div>
+    )
+  }
+  
+  export default NotFound
