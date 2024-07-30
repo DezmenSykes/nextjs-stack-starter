08a90cf96c5feba10c7ca8734353e92fac407162
DezmenSykes <oddly.observant@gmail.com>
2024-07-30 12:20:14 -0500

    Added basic styles and color palette.

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,8 +1,11 @@
+import Link from "next/link"
+
 const NotFound = () => {
     return (
         <div>
-            <h2>404</h2>
+            <h2>404 Not Found</h2>
             <p>Sorry, the requested page does not exist my guy.</p>
+            <Link href="/">Return Home</Link>
         </div>
     )
   }
