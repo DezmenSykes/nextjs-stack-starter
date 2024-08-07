e13765533e605545fc8a732d2d17f3c10efa31c6
DezmenSykes <oddly.observant@gmail.com>
2024-08-07 16:56:44 -0500

    Finished contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,23 @@
+import Image from 'next/image'
 import styles from './contact.module.css'
 
 const ContactPage = () => {
     return (
       <div className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image src="/contact.png" alt="" fill/>
+        </div>
+        <div className={styles.formContainer}>
+          <form action="" className={styles.form}>
+            <input type='text' placeholder='First & Last Name' />
+            <input type='email' placeholder='Email' />
+            <input type='number' placeholder='Phone' />
+            <textarea name="" cols="30" rows="10" placeholder="Message">
 
+            </textarea>
+            <button className={styles.submitBtn}>Send</button>
+          </form>
+        </div>
       </div>
     )
   }
