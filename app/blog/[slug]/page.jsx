925a22dda578826bdf9d10f0fbd167ef4e6e736b
DezmenSykes <oddly.observant@gmail.com>
2024-08-08 10:19:59 -0500

    Styling single post page.

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,6 +1,16 @@
+import Image from 'next/image'
+import styles from './single-post.module.css'
+
 const SinglePostPage = () => {
     return (
-      <h1>SinglePostPage!</h1>
+      <div className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image className={styles.img} src="/contact.png" alt="" fill/>
+        </div>
+        <div className={styles.postContainer}>
+          <h1>Title</h1>
+        </div>
+      </div>
     )
   }
   
