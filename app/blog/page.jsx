098f4ae3075b4a1b198a07baec6a777ebf0b4400
DezmenSykes <oddly.observant@gmail.com>
2024-08-09 15:21:09 -0500

    Added data fecthing to retrieve posts from DB.

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,26 +1,27 @@
+import { error } from 'console';
 import PostCard from '../../components/PostCard/PostCard'
 import styles from './blog.module.css'
-const BlogPage = () => {
+
+const getData = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+  if(!res.ok)
+    throw error("Something went wrong..")
+
+  return res.json()
+}
+
+const BlogPage = async () => {
+    const posts = await getData();
+
     return (
       <div className={styles.container}>
-        <div className={styles.post}>
-          <PostCard />
-        </div>
-        <div className={styles.post}>
-          <PostCard />
-        </div>
-        <div className={styles.post}>
-          <PostCard />
-        </div>
-        <div className={styles.post}>
-          <PostCard />
-        </div>
-        <div className={styles.post}>
-          <PostCard />
-        </div>
-        <div className={styles.post}>
-          <PostCard />
-        </div>
+        {posts.map((post) => {
+          <div className={styles.post} key={post.id}> 
+            <PostCard post={post} />
+          </div>
+        })}
+  
       </div>
     )
   }
--- components/PostCard/PostCard.jsx
+++ components/PostCard/PostCard.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import styles from './post-card.module.css'
 import Link from 'next/link'
-const PostCard = () => {
+const PostCard = ({post}) => {
     return (
         <div className={styles.container}>
             <div className={styles.top}>
@@ -11,8 +11,8 @@ const PostCard = () => {
                 <span className={styles.date}>08.07.2024</span>
             </div>
             <div className={styles.bottom}>
-                <h1 className={styles.title}>Title</h1>
-                <p className={styles.desc}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, nisi? Deserunt voluptatum dignissimos magni. Quo, ratione quia, doloribus enim, quibusdam explicabo itaque nihil nam veniam rerum soluta ipsa repellendus consequatur?</p>
+                <h1 className={styles.title}>{post.title}</h1>
+                <p className={styles.desc}>{post.description}</p>
                 <Link href="/blog/post" className={styles.readMoreBtn}>Read More</Link>
             </div>
         </div>
