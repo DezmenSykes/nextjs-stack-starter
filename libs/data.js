498ceae65244920f574e8beec3928b406f834a8c
DezmenSykes <oddly.observant@gmail.com>
2024-08-20 18:09:42 -0500

    Updated data fetching from db.

--- libs/data.js
+++ libs/data.js
@@ -1,14 +1,38 @@
-const posts = [
-    {id: 1, title: 'Post 1', body: '......', userId: 1},
-    {id: 2, title: 'Post 2', body: '......', userId: 1},
-    {id: 3, title: 'Post 3', body: '......', userId: 2},
-    {id: 4, title: 'Post 4', body: '......', userId: 2}
-]
+import { Post, User } from "./models"
+import { connectToDB } from "./utils";
 
-export const getPosts = () => {
-    return posts
+export const getPosts = async () => {
+    try {
+        connectToDB()
+
+        const posts = Post.find()
+        return posts;
+    } catch (error) {
+        console.log(error)
+        throw new Error("Failed to fetch post")
+    }
 }
 
-export const getPost = (id) => {
-    return posts.find((post) => {post.id == id})
+export const getPost = async (slug) => {
+    try {
+        connectToDB()
+
+        const post = Post.find(slug)
+        return post;
+    } catch (error) {
+        console.log(error)
+        throw new Error("Failed to fetch post")
+    }
+}
+
+export const getUser = async (id) => {
+    try {
+        connectToDB()
+
+        const user = User.find(id)
+        return user;
+    } catch (error) {
+        console.log(error)
+        throw new Error("Failed to fetch user")
+    }
 }
