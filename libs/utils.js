4fcc035fffff09294123372d1b7217efab9ad3e4
DezmenSykes <oddly.observant@gmail.com>
2024-09-04 16:28:47 -0500

    Database connection stuf.

--- libs/utils.js
+++ libs/utils.js
@@ -4,12 +4,14 @@ const connection = {}
 
 export const connectToDB = async () => {
     try {
-        if(mongoose.connections[0].readyState) {
+        if(connection.isConnected) {
             console.log('Using existing connection', mongoose.connections[0].readyState);
             return true;
         }
 
-        await mongoose.connect(process.env.MONGO_URL);
+        const db = await mongoose.connect(process.env.MONGO_URL);
+        connection.isConnected = db.connections[0].readyState;
+        
         console.log(connection, "Mongo is connected.");
         return true;
     } catch (error) {
