b1eb90d8be7ae64f58a1249180cbdb63e4c0baf0
Dezmen Sykes <dezmensykes.ds1@gmail.com>
2024-09-12 12:00:57 -0500

    added login styles

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,16 +1,19 @@
 import { loginWithGithub, loginWithCredentials } from "@/libs/actions"
+import styles from "./login.module.css"
 const LoginPage = async () => {
   return (
-      <div>
-        <h1>Login</h1>
-        <form action={loginWithCredentials}>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+        <h1 className={styles.title}>Login</h1>
+        <form className={styles.loginForm} action={loginWithCredentials}>
           <input type="text" name="username" placeholder="Username" />
           <input type="password" name="password" placeholder="Password" />
           <button>Login</button>
         </form>
-        <form action={loginWithGithub}>
+        <form className={styles.githubForm} action={loginWithGithub}>
           <button>Login With Github</button>
         </form>
+        </div>
       </div>
     )
 }
